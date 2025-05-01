ead55e5266312eb490238b878e92961bd1fb104c
ansari katib <ansarikatib647@gmail.com>
2025-05-01 12:13:06 +0530

    bubble sort logic check

--- new java prog/Array/bubbleSort.java
+++ new java prog/Array/bubbleSort.java
@@ -1,14 +1,24 @@
-package Array;
 
+import java.util.Arrays;
 public class bubbleSort {
     public static void main(String[] args) {
-        int[] arr = {1,8,5,9,4,3,7};
-        System.out.println(BubbbleSort(arr));
+
+        int[] arr = { 40, 30, 20, 45, 35, 10 };
+        int arr_length = arr.length;
+
+        for (int i = 0; i < arr_length - 1; i++) {
+            for (int j = 0; j < arr_length - i - 1; j++) {
+                
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted Array: " + Arrays.toString(arr));  
+
     }
 
-    public static int BubbbleSort(int[] arr){
-        for(int i=0 ; i< arr.length ; i++){
-            System.out.println(arr[i]);
-        }
-    }
 }
